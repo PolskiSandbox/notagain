21bdd916b4dd66eb8ac1a501982d2afb1ed8912a
SpiralP <spiral.palmer@gmail.com>
2018-07-23 18:19:58 -0700

    bump version to latest goluwa

--- lua/notagain/goluwa/goluwa.lua
+++ lua/notagain/goluwa/goluwa.lua
@@ -1023,7 +1023,7 @@ function goluwa.SetEnv()
 end
 
 if CLIENT then
-	goluwa.Update(goluwa.Initialize, "1d750df2")
+	goluwa.Update(goluwa.Initialize, "04f113c9")
 end
 
 if game.IsDedicated() or CLIENT then
